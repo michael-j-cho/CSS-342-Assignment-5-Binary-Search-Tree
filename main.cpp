597aa8f4a7610463146afdb12e25dc5f24d602f2
Tim Denisiuk <tdenis987@gmail.com>
2021-03-06 13:09:42 -0800

    Added test to test removing 0, 1, and 2 children

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /**
  * Michael Cho
  * Tim D
- * 
+ *
  * CSS342
  *
  * This is the main driver file that is running the tests.
@@ -16,13 +16,35 @@ using namespace std;
 void test1() {
     ThreadedBST tree2(20);
     // cout << endl;
-     ThreadedBST tree3(21);
+    ThreadedBST tree3(21);
     // tree2.remove(1);
 
 }
 
+void removetest()
+{
+    ThreadedBST tree1;
+    tree1.add(10);
+    tree1.add(15);
+    tree1.add(5);
+    tree1.add(3);
+    tree1.add(7);
+    tree1.add(12);
+    tree1.add(9);
+    tree1.add(6);
+    tree1.add(8);
+    tree1.add(11);
+    tree1.remove(8);
+    assert(!tree1.contains(8));
+    tree1.remove(12);
+    assert(!tree1.contains(12));
+    tree1.remove(7);
+    assert(!tree1.contains(7));
+}
+
 int main() {
-  test1();
-  cout << endl << "Done." << endl;
-  return 0;
+    //test1();
+    removetest();
+    cout << endl << "Done." << endl;
+    return 0;
 }
