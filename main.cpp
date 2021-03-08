2d6772b42e7e240645ed6e97953f65937718c969
Michael Cho <mychael.cho@gmail.com>
2021-03-08 13:02:27 -0800

    Formatted cpp files with Clang. Added sh scripts.

--- main.cpp
+++ main.cpp
@@ -15,63 +15,64 @@ using namespace std;
 
 // Testing constructor, add, and contains methods.
 void test1() {
-    ThreadedBST tree1(20);
-    assert(!tree1.contains(21));
-    tree1.add(21);
-    assert(tree1.contains(20));
-    cout << endl << endl;
+  cout << "Running test1: Testing add and contains" << endl;
+  ThreadedBST tree1(20);
+  assert(!tree1.contains(21));
+  tree1.add(21);
+  assert(tree1.contains(20));
+  cout << endl << endl;
 }
 
 // Testing copy constuctor
 void test2() {
-    ThreadedBST tree2(20);
-    cout << endl << endl;
-    ThreadedBST tree3(tree2);
+  cout << "Running test2: Testing copy constructor" << endl;
+  ThreadedBST tree2(20);
+  cout << endl;
+  ThreadedBST tree3(tree2);
+  cout << endl << endl;
 }
 
-void removetest()
-{
-    ThreadedBST tree1;
-    tree1.add(10);
-    tree1.add(15);
-    tree1.add(5);
-    tree1.add(3);
-    tree1.add(7);
-    tree1.add(12);
-    tree1.add(9);
-    tree1.add(6);
-    tree1.add(8);
-    tree1.add(11);
-    /* tree1.remove(8);
-     assert(!tree1.contains(8));
-     tree1.remove(12);
-     assert(!tree1.contains(12));
-     tree1.remove(7);
-     assert(!tree1.contains(7));*/
-    tree1.remove(10);
+void removetest() {
+  ThreadedBST tree1;
+  tree1.add(10);
+  tree1.add(15);
+  tree1.add(5);
+  tree1.add(3);
+  tree1.add(7);
+  tree1.add(12);
+  tree1.add(9);
+  tree1.add(6);
+  tree1.add(8);
+  tree1.add(11);
+  /* tree1.remove(8);
+   assert(!tree1.contains(8));
+   tree1.remove(12);
+   assert(!tree1.contains(12));
+   tree1.remove(7);
+   assert(!tree1.contains(7));*/
+  tree1.remove(10);
 }
 
-void threadtest()
-{
-    ThreadedBST tree1;
-    tree1.add(6);
-    tree1.add(3);
-    tree1.add(1);
-    tree1.add(2);
-    tree1.add(5);
-    tree1.add(8);
-    tree1.add(7);
-    tree1.add(11);
-    tree1.add(9);
-    tree1.add(13);
-    tree1.thread();
+void threadtest() {
+  ThreadedBST tree1;
+  tree1.add(6);
+  tree1.add(3);
+  tree1.add(1);
+  tree1.add(2);
+  tree1.add(5);
+  tree1.add(8);
+  tree1.add(7);
+  tree1.add(11);
+  tree1.add(9);
+  tree1.add(13);
+  tree1.thread();
 }
 
 int main() {
-    //test1();
-    //test2();
-    //removetest();
-    threadtest();
-    cout << endl << "Done." << endl;
-    return 0;
+  test1();
+  test2();
+  removetest();
+  threadtest();
+  cout << endl << "Done." << endl;
+  return 0;
 }
